1d89b7d9f630d3b618a9e78ba56f373db64dd486
patmejia <92187562+patmejia@users.noreply.github.com>
2024-06-17 13:04:09 -0700

    feat: hamburger nav

--- js/script.js
+++ js/script.js
@@ -1,20 +1,23 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const images = document.querySelectorAll(".dog-image");
-  const navToggleLabel = document.getElementById("nav-toggle-label");
-  const navMenu = document.getElementById("nav-menu");
-
+document.addEventListener('DOMContentLoaded', function() {
+  const images = document.querySelectorAll('.dog-image');
+  
   images.forEach(image => {
-    image.addEventListener("click", function() {
-      if (this.classList.contains("expanded")) {
-        this.classList.remove("expanded");
+    image.addEventListener('click', function() {
+      if (this.classList.contains('expanded')) {
+        this.classList.remove('expanded');
       } else {
-        images.forEach(img => img.classList.remove("expanded"));
-        this.classList.add("expanded");
+        images.forEach(img => img.classList.remove('expanded'));
+        this.classList.add('expanded');
       }
     });
   });
+});
 
-  navToggleLabel.addEventListener("click", function() {
-    navMenu.classList.toggle("active");
+document.addEventListener('DOMContentLoaded', function() {
+  const navToggleLabel = document.getElementById('nav-toggle-label');
+  const navMenu = document.getElementById('nav-menu');
+  
+  navToggleLabel.addEventListener('click', function() {
+    navMenu.classList.toggle('active');
   });
 });
