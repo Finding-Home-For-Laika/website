d82a97537a35f687c8fbea59bf8b7c35f639db11
patmejia <92187562+patmejia@users.noreply.github.com>
2024-07-12 17:34:37 -0700

    Add Babel config and update package fil

--- babel.config.js
+++ babel.config.js
@@ -2,10 +2,12 @@ module.exports = function (api) {
   api.cache(true);
 
   const presets = ["@babel/preset-env"];
+  const plugins = ["@babel/plugin-proposal-logical-assignment-operators"];
   const ignore = ["node_modules"];
 
   return {
     presets,
+    plugins,
     ignore,
   };
 };
