1f1374279ed695a8b573eae98b25482f11aefe29
patmejia <92187562+patmejia@users.noreply.github.com>
2024-07-12 17:18:53 -0700

    Add Babel config and update package files

---
+++ babel.config.js
@@ -0,0 +1,11 @@
+module.exports = function (api) {
+  api.cache(true);
+
+  const presets = ["@babel/preset-env"];
+  const ignore = ["node_modules"];
+
+  return {
+    presets,
+    ignore,
+  };
+};
